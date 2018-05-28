bb11888deb20c985abe6f143f7994116ae7fb05b
ULAGOSrcaro <37742694+ULAGOSrcaro@users.noreply.github.com>
2018-05-28 00:32:35 -0400

    Estructura y clases
    Se creo la estructura de carpetas y clases

---
+++ MVC_Panderia/Controllers/precio_ventaController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Panderia.Controllers
+{
+    public class precio_ventaController
+    {
+    }
+}
