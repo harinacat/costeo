0614d879151cbd9a9a96d7c67c377e70ca609a47
gaston-barrientos <mastergastonb@gmail.com>
2018-05-30 23:00:31 -0400

    linea, listar y crear

--- MVC_Panderia/Controllers/lineaController.cs
+++ MVC_Panderia/Controllers/lineaController.cs
@@ -2,10 +2,107 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using MVC_Panderia.Models;
+
 
 namespace MVC_Panderia.Controllers
 {
-    public class lineaController
-    {
+    public class lineaController:Controller
+     {
+        // GET: Linea
+        public ActionResult Index()
+        {
+            pan_dbEntities1 db = new pan_dbEntities1();
+            var lineas = db.lineas.ToList();
+            ViewBag.lineas = lineas;
+            return View();
+        }
+
+        // GET: Linea/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Linea/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Linea/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                pan_dbEntities1 db = new pan_dbEntities1();
+                linea ln = new linea();
+                ln.nombre = collection.Get("nombre");
+                db.lineas.Add(ln);
+                db.SaveChanges();
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Linea/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Linea/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                pan_dbEntities1 db = new pan_dbEntities1();
+                linea ln = new linea();
+                ln = db.lineas.Find(Convert.ToInt16(collection.Get("id")));
+                ln.nombre = collection.Get("nombre");
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Linea/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Linea/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
     }
 }
--- MVC_Panderia/Models/modeloCosteos.edmx.sql
+++ MVC_Panderia/Models/modeloCosteos.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 05/19/2018 11:05:09
--- Generated from EDMX file: D:\AKVA_Rodrigo\ULagos\Quinto Semestre\Taller de pruebas\2 Evaluacion\4 - Desarrollo\MVC_Panderia\Models\PanModel.edmx
+-- Date Created: 05/30/2018 22:43:28
+-- Generated from EDMX file: C:\Users\GBARRIENTOS\Documents\GitHub\costeo\MVC_Panderia\Models\modeloCosteos.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
@@ -27,22 +27,8 @@ GO
 -- Creating all tables
 -- --------------------------------------------------
 
--- Creating table 'unidad_medidaSet'
-CREATE TABLE [dbo].[unidad_medidaSet] (
-    [Id] int IDENTITY(1,1) NOT NULL,
-    [nombre] nvarchar(max)  NOT NULL
-);
-GO
-
--- Creating table 'lineaSet'
-CREATE TABLE [dbo].[lineaSet] (
-    [Id] int IDENTITY(1,1) NOT NULL,
-    [nombre] nvarchar(max)  NOT NULL
-);
-GO
-
--- Creating table 'familiaSet'
-CREATE TABLE [dbo].[familiaSet] (
+-- Creating table 'lineas'
+CREATE TABLE [dbo].[lineas] (
     [Id] int IDENTITY(1,1) NOT NULL,
     [nombre] nvarchar(max)  NOT NULL
 );
@@ -52,21 +38,9 @@ GO
 -- Creating all PRIMARY KEY constraints
 -- --------------------------------------------------
 
--- Creating primary key on [Id] in table 'unidad_medidaSet'
-ALTER TABLE [dbo].[unidad_medidaSet]
-ADD CONSTRAINT [PK_unidad_medidaSet]
-    PRIMARY KEY CLUSTERED ([Id] ASC);
-GO
-
--- Creating primary key on [Id] in table 'lineaSet'
-ALTER TABLE [dbo].[lineaSet]
-ADD CONSTRAINT [PK_lineaSet]
-    PRIMARY KEY CLUSTERED ([Id] ASC);
-GO
-
--- Creating primary key on [Id] in table 'familiaSet'
-ALTER TABLE [dbo].[familiaSet]
-ADD CONSTRAINT [PK_familiaSet]
+-- Creating primary key on [Id] in table 'lineas'
+ALTER TABLE [dbo].[lineas]
+ADD CONSTRAINT [PK_lineas]
     PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
