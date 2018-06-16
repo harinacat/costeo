8732dc2fa6ce8dc50361d7ee65eaa5c7805d1b2c
freddy <freddy.anzu@gmail.com>
2018-06-16 11:04:13 -0400

    correccion de usuario
    borre y volvi a crear la tabla usuario

--- dbo.usuarios.data.sql
+++ dbo.usuarios.data.sql
@@ -1,2 +1,1 @@
-﻿INSERT INTO [dbo].[usuarios] ([Id], [nombre_usuario], [contraseña], [email]) VALUES (N'12345', N'freddy', N'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', N'fantipa@gmail.com')
-
+﻿INSERT INTO [dbo].[usuarios] ([Id], [nombre_usuario], [contraseña], [email]) VALUES (16506523,'fantipa', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'fantipa@gmail.com')
