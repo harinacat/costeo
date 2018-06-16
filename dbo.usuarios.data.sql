e64e7d7619198dc8a468ff4ace8b9eb495a5019e
freddy <freddy.anzu@gmail.com>
2018-06-16 10:48:47 -0400

    se creo tabla usuario
    se ingreso datos para usuario

---
+++ dbo.usuarios.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[usuarios] ([Id], [nombre_usuario], [contraseña], [email]) VALUES (N'12345', N'freddy', N'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', N'fantipa@gmail.com')
+
---
+++ tabla-usuario.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[usuarios] ([Id], [nombre_usuario], [contraseña], [email]) VALUES (16506523,'fantipa', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3','freddy.anzu@gmail.com') 
