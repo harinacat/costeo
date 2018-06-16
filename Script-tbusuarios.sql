86f92b0e8c60a504d8f6213517900a7b5b200cc0
gaston-barrientos <mastergastonb@gmail.com>
2018-06-16 10:14:48 -0400

    script
    Script usuarios

---
+++ Script-tbusuarios.sql
@@ -0,0 +1,7 @@
+﻿-- Creating table 'usuarios'
+CREATE TABLE [dbo].[usuarios] (
+    [Id] nvarchar(50)  NOT NULL,
+    [nombre_usuario] nvarchar(150)  NOT NULL,
+    [contraseña] nvarchar(max)  NOT NULL,
+    [email] nvarchar(50)  NOT NULL
+);
