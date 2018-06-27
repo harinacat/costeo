97bd89c734ba775df53ce2d5259433aa24e2c94d
ULAGOSrcaro <37742694+ULAGOSrcaro@users.noreply.github.com>
2018-06-27 19:47:18 -0400

    Se crea Crud produccion
    Se crea el crud de la produccion y detalle. Queda pendiente desplegar la receta en el detalle.

--- MVC_Panderia/Controllers/cabecera_produccionController.cs
+++ MVC_Panderia/Controllers/cabecera_produccionController.cs
@@ -37,7 +37,6 @@ namespace MVC_Panderia.Controllers
             try
             {
                 // TODO: Add insert logic here
-                pan_dbEntities db = new pan_dbEntities();
                 cabecera_produccion ln = new cabecera_produccion();
                 ln.fecha = Convert.ToDateTime(collection.Get("fecha"));
                 db.cabecera_produccion.Add(ln);
@@ -55,7 +54,6 @@ namespace MVC_Panderia.Controllers
         // GET: Linea/Edit/5
         public ActionResult Edit(int id)
         {
-            pan_dbEntities db = new pan_dbEntities();
             var Row = db.cabecera_produccion.Where(s => s.Id == id).FirstOrDefault();
             return View(Row);
         }
@@ -67,7 +65,6 @@ namespace MVC_Panderia.Controllers
             try
             {
                 // TODO: Add update logic here
-                pan_dbEntities db = new pan_dbEntities();
                 cabecera_produccion ln = new cabecera_produccion();
                 ln = db.cabecera_produccion.Find(Convert.ToInt16(collection.Get("Id")));
                 ln.fecha = Convert.ToDateTime(collection.Get("fecha"));
@@ -84,7 +81,6 @@ namespace MVC_Panderia.Controllers
         // GET: Linea/Delete/5
         public ActionResult Delete(int id)
         {
-            pan_dbEntities db = new pan_dbEntities();
             var Row = db.cabecera_produccion.Where(s => s.Id == id).FirstOrDefault();
             return View(Row);
         }
@@ -95,7 +91,6 @@ namespace MVC_Panderia.Controllers
         {
             try
             {
-                pan_dbEntities db = new pan_dbEntities();
                 cabecera_produccion ln = new cabecera_produccion();
                 ln = db.cabecera_produccion.Find(Convert.ToInt16(collection.Get("id")));
                 db.cabecera_produccion.Remove(ln);
@@ -112,10 +107,108 @@ namespace MVC_Panderia.Controllers
         // GET: cabecera_produccion
         public ActionResult IndexDetail(int id, string nombre)
         {
+            cabecera_produccion ln = new cabecera_produccion();
+            ln = db.cabecera_produccion.Find(id);
+            ViewBag.Fecha = ln.fecha.ToShortDateString();
             ViewBag.cabecera_produccionId = id;
-            return View(db.detalle_produccion.Where(s => s.cabecera_produccionId == id));
+            var Row = db.detalle_produccion.Where(s => s.cabecera_produccionId == id);
+            return View(Row);
         }
 
+        // GET: DetalleProduccion/Create
+        public ActionResult CreateDetail(int id, string nombre)
+        {
+            ViewBag.cabecera_recetaId = new SelectList(db.cabecera_receta,"Id", "Id");
+            return View();
+        }
 
+        // POST: DetalleProduccion/CreateDetail
+        [HttpPost]
+        public ActionResult CreateDetail(FormCollection collection, int id)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                detalle_produccion dr = new detalle_produccion();
+                dr.kilos = Convert.ToInt32(collection.Get("kilos"));
+                dr.cabecera_recetaId = Convert.ToInt16(collection.Get("cabecera_recetaId"));
+                dr.cabecera_produccionId = id;
+                var RowCabeceraProduccion = db.cabecera_produccion.Where(s => s.Id == dr.cabecera_produccionId).First();
+                var RowCosto = db.costo.Where(s => s.cabecera_recetaId == dr.cabecera_recetaId && s.fecha <= RowCabeceraProduccion.fecha).OrderByDescending(s => s.fecha).First();
+                var RowVenta = db.precio_venta.Where(s => s.cabecera_recetaId == dr.cabecera_recetaId && s.fecha <= RowCabeceraProduccion.fecha).OrderByDescending(s => s.fecha).First();
+                dr.fechacosto = RowCosto.fecha;
+                dr.fechaventa = RowVenta.fecha;
+                db.detalle_produccion.Add(dr);
+                db.SaveChanges();
+                return RedirectToAction("IndexDetail", new { id = id});
+            }
+            catch(Exception exp)
+            {
+                return View();
+            }
+        }
+
+        // GET: detalle_produccion/Edit/5
+        public ActionResult EditDetail(int id, int id_cabecera)
+        {
+            ViewBag.cabecera_produccionId = id_cabecera;
+            var Row = db.detalle_produccion.Where(s => s.Id == id).FirstOrDefault();
+            ViewBag.cabecera_recetaId = new SelectList(db.cabecera_receta, "Id", "Id", Row.cabecera_recetaId);
+            return View(Row);
+        }
+
+        // POST: detalle_receta/Edit/5
+        [HttpPost]
+        public ActionResult EditDetail(FormCollection collection, int id, int id_cabecera)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                detalle_produccion dr = new detalle_produccion();
+                dr = db.detalle_produccion.Find(id);
+
+                dr.kilos = Convert.ToDecimal(collection.Get("kilos"));
+                dr.cabecera_recetaId = Convert.ToInt16(collection.Get("cabecera_recetaId"));
+                var RowCabeceraProduccion = db.cabecera_produccion.Where(s => s.Id == dr.cabecera_produccionId).First();
+                var RowCosto = db.costo.Where(s => s.cabecera_recetaId == dr.cabecera_recetaId && s.fecha <= RowCabeceraProduccion.fecha).OrderByDescending(s => s.fecha).First();
+                var RowVenta = db.precio_venta.Where(s => s.cabecera_recetaId == dr.cabecera_recetaId && s.fecha <= RowCabeceraProduccion.fecha).OrderByDescending(s => s.fecha).First();
+                dr.fechacosto = RowCosto.fecha;
+                dr.fechaventa = RowVenta.fecha;
+                db.SaveChanges();
+
+                return RedirectToAction("IndexDetail", new { id = id_cabecera});
+            }
+            catch(Exception exp)
+            {
+                return View();
+            }
+        }
+
+        // GET: detalle_receta/Delete/5
+        public ActionResult DeleteDetail(int id, int id_cabecera)
+        {
+            ViewBag.cabecera_produccionId = id_cabecera;
+            var Row = db.detalle_produccion.Where(s => s.Id == id).FirstOrDefault();
+            ViewBag.cabecera_recetaId = new SelectList(db.cabecera_receta, "Id", "Id", Row.cabecera_recetaId);
+            return View(Row);
+        }
+
+        // POST: detalle_receta/Delete/5
+        [HttpPost]
+        public ActionResult DeleteDetail(FormCollection collection, int id, int id_cabecera, string nombre)
+        {
+            try
+            {
+                detalle_produccion cr = new detalle_produccion();
+                cr = db.detalle_produccion.Find(Convert.ToInt16(collection.Get("id")));
+                db.detalle_produccion.Remove(cr);
+                db.SaveChanges();
+                return RedirectToAction("IndexDetail", new { id = id_cabecera});
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
