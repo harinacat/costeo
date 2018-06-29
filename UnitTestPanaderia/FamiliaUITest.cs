dadc892fb2ca344ea2e49821020f4ce731cb1351
gaboxx28 <gabrielvargasigor@gmail.com>
2018-06-29 20:34:36 -0300

    familiUITest
    Test agregar, editar, eliminar

--- UnitTestPanaderia/FamiliaUITest.cs
+++ UnitTestPanaderia/FamiliaUITest.cs
@@ -22,53 +22,53 @@ namespace UITestProject
             //logearse
             driver.Navigate().GoToUrl(url + "/usuario/Login?ReturnUrl=%2f");
             driver.FindElement(By.Name("Id")).SendKeys("gvargas");
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
             driver.FindElement(By.Name("contrasena")).SendKeys("test");
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
             driver.FindElement(By.Id("login")).Click();
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
 
             //Ir a index familia y agregar familia
             driver.Navigate().GoToUrl(url + "/familia");
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("btn-nuevo")).Click();
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("/html/body/div[2]/div[1]/div[2]/a")).Click();
+            Thread.Sleep(1000);
             driver.FindElement(By.Name("nombre")).SendKeys("Test");
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("ddl_linea")).Click();
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("ddl_linea")).SendKeys(Keys.Down );
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("ddl_linea")).SendKeys(Keys.Enter);
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("btn-guardar")).Click();
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
+            driver.FindElement(By.Name("lineaId")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.Name("lineaId")).SendKeys(Keys.Down );
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("/html/body/div[2]/form/button[1]")).Click();
+            Thread.Sleep(1000);
 
             //Editar primer registro 
             IWebElement baseTable1 = driver.FindElement(By.TagName("table"));
             IWebElement tableRow1 = baseTable1.FindElement(By.XPath("/html/body/div[2]/div[2]/div/table/tbody/tr[8]"));
-            tableRow1.FindElement(By.Id("btn-editar")).Click();
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("txt_nombre_editar")).Clear();
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("txt_nombre_editar")).SendKeys("familiaTest");
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("ddl_linea_editar")).SendKeys(Keys.Down + Keys.Enter);
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("ddl_linea_editar")).SendKeys(Keys.Enter);
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("btn_guardar_edicion")).Click();
-            Thread.Sleep(2000);
+            tableRow1.FindElement(By.XPath("/html/body/div[2]/div[2]/div/table/tbody/tr[8]/td[4]/a[1]")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.Name("nombre")).Clear();
+            Thread.Sleep(1000);
+            driver.FindElement(By.Name("nombre")).SendKeys("familiaTest");
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Down + Keys.Enter);
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("/html/body/div[2]/form/button[1]")).Click();
+            Thread.Sleep(1000);
 
             
             
             //Eliminar registro N°8 agregado
             IWebElement baseTable = driver.FindElement(By.TagName("table"));
             IWebElement tableRow = baseTable.FindElement(By.XPath("/html/body/div[2]/div[2]/div/table/tbody/tr[8]"));
-            tableRow.FindElement(By.Id("btn-eliminar")).Click();
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("btn-eliminar2")).Click();
-            Thread.Sleep(2000);
+            tableRow.FindElement(By.XPath("/html/body/div[2]/div[2]/div/table/tbody/tr[8]/td[4]/a[2]")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("/html/body/div[2]/form/button[1]")).Click();
+            Thread.Sleep(1000);
 
 
         }
