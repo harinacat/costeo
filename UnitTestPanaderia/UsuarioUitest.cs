7c2a7a5fd9a4d826765506285b00c61d60a0e7fd
gaston-barrientos <mastergastonb@gmail.com>
2018-06-29 22:08:36 -0400

    usuariotest
    actualizar

--- UnitTestPanaderia/UsuarioUitest.cs
+++ UnitTestPanaderia/UsuarioUitest.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MVC_Panderia.Controllers;
 
 namespace CUnitTestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class UsuarioUITest
     {
         [TestMethod]
         public void TestMethod1()
