eb722acd520abd48552c8092dad5429dc2f133cb
gaboxx28 <gabrielvargasigor@gmail.com>
2018-07-03 09:20:27 -0300

    Correccion pruebas Familia
    Correcion de pruebas vistas

--- UnitTestPanaderia/FamiliaUITest.cs
+++ UnitTestPanaderia/FamiliaUITest.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Chrome;
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
+using System.Linq;
 
 namespace UITestProject
 {
@@ -29,37 +30,49 @@ namespace UITestProject
         [Test]
         public void Agregar() {
             _Login();
-       
-        //Ir a index familia y agregar familia
-        driver.Navigate().GoToUrl(url + "/familia");
-        driver.FindElement(By.Id("btn-nuevo-index")).Click();
-        driver.FindElement(By.Name("nombre")).SendKeys("Test");
-        driver.FindElement(By.Name("lineaId")).Click();
-        driver.FindElement(By.Name("lineaId")).SendKeys(Keys.Down);
-        driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Enter);
-        driver.FindElement(By.Id("btn-guardar-create")).Click();
-         }
+
+            //Ir a index familia y agregar familia
+            MVC_Panderia.Models.pan_dbEntities db = new MVC_Panderia.Models.pan_dbEntities();
+           
+            int FilasActuales = db.familia.Count();
+            driver.Navigate().GoToUrl(url + "/familia");
+            driver.FindElement(By.Id("btn-nuevo-index")).Click();
+            driver.FindElement(By.Name("nombre")).SendKeys("Test");
+            driver.FindElement(By.Name("lineaId")).Click();
+            driver.FindElement(By.Name("lineaId")).SendKeys(Keys.Down);
+            driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Enter);
+            driver.FindElement(By.Id("btn-guardar-create")).Click();
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(FilasActuales + 1, db.familia.Count());
+        }
 
         [Test]
         public void Editar() {
 
-        driver.FindElement(By.Id("btn-editar-index")).Click();
-        driver.FindElement(By.Name("nombre")).Clear();
-        driver.FindElement(By.Name("nombre")).SendKeys("familiaTest");
-        Thread.Sleep(1000);
-        driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Down + Keys.Enter);
-        Thread.Sleep(1000);
-        driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Enter);
-        driver.FindElement(By.Id("btn-guardar-editar")).Click();
+            driver.FindElement(By.Id("btn-editar-index")).Click();
+            driver.FindElement(By.Name("nombre")).Clear();
+            driver.FindElement(By.Name("nombre")).SendKeys("familiaTest");
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Down + Keys.Enter);
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("lineaId")).SendKeys(Keys.Enter);
+            driver.FindElement(By.Id("btn-guardar-editar")).Click();
+
+            MVC_Panderia.Models.pan_dbEntities db = new MVC_Panderia.Models.pan_dbEntities();
+            string nombre_familia = db.familia.ToList().OrderByDescending(s => s.Id).First().nombre;
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(nombre_familia, "familiaTest");
         }
 
-            [Test]
+        [Test]
             public void Eliminar() {
-           
+            MVC_Panderia.Models.pan_dbEntities db = new MVC_Panderia.Models.pan_dbEntities();
+            ////Obtiene el numero de recetas actuales
+            int FilasActuales = db.familia.Count();
             driver.FindElement(By.Id("btn-delete-index")).Click();
             driver.FindElement(By.Id("btn-eliminar-delete")).Click();
             Thread.Sleep(1000);
-            }
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(FilasActuales - 1, db.familia.Count());
+        }
             [Test]
             public void Listar() {
 
@@ -69,7 +82,7 @@ namespace UITestProject
              }
 
         [Test]
-        public void Verificar()
+        public void VerificarImg()
         {
             _Login();
             IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/div/img"));
